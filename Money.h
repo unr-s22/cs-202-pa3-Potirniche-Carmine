a932b0058c9b714b4182410ec215a61ff0236399
Carmine Potirniche <carminepotirniche@nevada.unr.edu>
2022-03-29 16:35:08 -0700

    Just created them working on them more tonight

---
+++ Money.h
@@ -0,0 +1,11 @@
+#ifndef MONEY_H
+#define MONEY_H
+#include <iostream>
+#include <vector>
+
+
+class Money {
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "Money.h"
+#include "Account.h"
+
