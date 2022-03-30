ce40aabc8a5fcb9ce822499244231600530d4637
Carmine Potirniche <carminepotirniche@nevada.unr.edu>
2022-03-30 16:48:29 -0700

    Money.h is done

--- Money.h
+++ Money.h
@@ -1,11 +1,34 @@
 #ifndef MONEY_H
 #define MONEY_H
 #include <iostream>
-#include <vector>
 
+using namespace std;
 
 class Money {
+private:
+    int cents, dollars;
 
+public:
+Money();
+
+Money(int dollarsIn, int centsIn);
+
+int getDollars();
+int getCents();
+
+void dollarsActual(int dollars);
+void centsActual(int cents);
+
+friend bool operator == (const Money&, const Money&);
+friend bool operator < (const Money&, const Money&);
+friend bool operator > (const Money&, const Money&);
+friend bool operator >= (const Money&, const Money&);
+friend bool operator <= (const Money&, const Money&);
+friend bool operator != (const Money&, const Money&);
+friend bool operator + (const Money&, const Money&);
+friend bool operator - (const Money&, const Money&);
+
+friend ostream& operator << (ostream &stream, const Money &totalMoney);
 };
 
 #endif
