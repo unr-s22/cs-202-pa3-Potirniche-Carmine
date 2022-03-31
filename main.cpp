0692cddb18e1f52d6337060bef8145f31ce3c646
1105-Dodge-Dillon <ddodge@nevada.unr.edu>
2022-03-30 19:58:37 -0700

    what do you think of the main.cpp so far?

--- main.cpp
+++ main.cpp
@@ -2,3 +2,26 @@
 #include "Money.h"
 #include "Account.h"
 
+int main()
+{
+    Money initial(300, 23);
+    Account myAccount(initial*);
+
+    std::cout << myAccount << std::endl;
+
+    Money firstDeposit(200, 0);
+    myAccount.makeDeposit(firstDeposit);
+    Money secondDeposit(300, 24);
+    myAccount.makeDeposit(secondDeposit);
+    Money thirdDeposit(501, 22);
+    myAccount.makeDeposit(thirdDeposit);
+
+    std::cout << myAccount << std::endl;
+
+    Money firstWithdrawal(300, 10);
+    myAccount.makeWithdrawals(firstWithdrawal);
+    Money secondWithdrawal(201, 34);
+    myAccount.makeWithdrawals(secondWithdrawal);
+    
+    return 0;
+}
